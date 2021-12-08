01d5571ace8ad764935103372b2a6c06e5602840
dsuccar <danielsuccar@icloud.com>
2021-12-08 16:03:39 -0500

    git activity controller

---
+++ app/controllers/git_activity_controller.rb
@@ -0,0 +1,14 @@
+class GitActivityController < ApplicationController
+
+  def index
+    gitActivities = GitActivity.all
+    render json: gitActivities.to_json
+  end
+
+  def show
+    gitActivity = GitActivity.find(params[:id])
+    render json: gitActivity
+  end
+    
+
+end
---
+++ test/controllers/git_activity_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GitActivityControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
